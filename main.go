fd3f333d76dce72bad90dfeb56c5f779dfe63a5e
irahardianto <ichsan.rahardianto@gmail.com>
2018-07-07 10:55:06 +0700

    working implementation of recognition

--- main.go
+++ main.go
@@ -8,18 +8,17 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/rekognition"
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+//Handler is used to handle Lambda event
 func Handler(ctx context.Context, s3Event events.S3Event) {
-	sess, err := session.NewSession(&aws.Config{
+	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("ap-northeast-1")},
 	)
 
 	// Create DynamoDB client
-	awsDynamoDB := dynamodb.New(sess)
+	//awsDynamoDB := dynamodb.New(sess)
 	awsRekognition := rekognition.New(sess)
 
 	for _, record := range s3Event.Records {
@@ -27,17 +26,18 @@ func Handler(ctx context.Context, s3Event events.S3Event) {
 		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
 		fmt.Printf("Hello From Go")
 
-	}
-
-	imageToParse := rekognition.DetectTextInput{
-		Image: &rekognition.Image{
-			S3Object: &s3.Object{
-				Bucket: "",
-				Name:   "",
+		imageToParse := rekognition.DetectTextInput{
+			Image: &rekognition.Image{
+				S3Object: &rekognition.S3Object{
+					Bucket: &s3.Bucket.Name,
+					Name:   &s3.Object.Key,
+				},
 			},
-		},
+		}
+		plateNumber, _ := awsRekognition.DetectText(&imageToParse)
+
+		println(plateNumber.String)
 	}
-	plateNumber, _ := awsRekognition.DetectText(&imageToParse)
 }
 
 func main() {
